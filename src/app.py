971771d25ce283aff0c361f907fe7e878ee4ab1b
jerwansyah <blcktmyie123@gmail.com>
2021-04-05 02:18:42 +0700

    updated front-end

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 import plotly.graph_objects as go
+from astar import *
+
 app = Flask(__name__)
+Bootstrap(app)
 
 def createMap():
     fig = go.Figure(go.Scattermapbox(
@@ -20,10 +23,22 @@ def createMap():
 
     fig.write_html("templates/check.html")
 
+# @app.route('/', methods=['POST, GET'])
+# def browse():
+#     filename = 'yee'
+#     return render_template('main.html', filename=filename)
+
+@app.route('/', methods=['GET'])
+def dropdown():
+    result = makeGraph(parse("test.txt"))
+    nodeA = [i.name[0] for i in result]
+    nodeB = [i.name[0] for i in result]
+    return render_template('main.html', nodeA=nodeA, nodeB=nodeB)
+
 @app.route('/')
 def route():
     createMap()
-    return render_template('check.html')
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=2211)
