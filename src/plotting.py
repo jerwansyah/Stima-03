51686d6b4c05253763171aae6d1d732dc21f353b
Zenovore <agustinus.alexander17@gmail.com>
2021-04-05 16:16:39 +0700

    add plotting

---
+++ src/plotting.py
@@ -0,0 +1,65 @@
+import chart_studio
+import chart_studio.plotly as py
+import plotly.graph_objects as go
+
+from astar import *
+
+chart_studio.tools.set_credentials_file(username='Zenovore', api_key='WVo1aIq79gDxkydrcXbn')
+
+def createMap(graph):
+    lintang =[]
+    bujur =[]
+    fig = go.Figure(go.Scattermapbox(
+        mode = "markers+lines",
+        lon = [],
+        lat = [],
+        marker = {'size': 10}))
+    result = astar(graph[1],graph[19],graph)
+    print(result)
+    for i in graph:
+        lintang.append(i.name[2])
+        bujur.append(i.name[1])
+        for x,y in i.adjacentNodes.items():
+            if (i.name[0] in result and x[0] in result):
+                fig.add_trace(go.Scattermapbox(
+                    mode = "lines",
+                    lon = [i.name[2],x[2]],
+                    lat = [i.name[1],x[1]],
+                    showlegend = False,
+                    hoverinfo = "none",
+                    line_color = '#FFFFFF'))
+            else:
+                fig.add_trace(go.Scattermapbox(
+                    mode = "lines",
+                    lon = [i.name[2],x[2]],
+                    lat = [i.name[1],x[1]],
+                    showlegend = False,
+                    hoverinfo = "none",
+                    line_color = '#000000'))
+    for i in graph:
+        fig.add_trace(go.Scattermapbox(
+            mode = "markers",
+            lon = [i.name[2]],
+            lat = [i.name[1]],
+            name = i.name[0],
+            hoverinfo = 'name',
+            marker_color = '#000000',
+            marker = {'size':20}))
+
+    # fig.update_traces(marker_color = "#00000")
+
+    fig.update_layout(
+        margin ={'l':0,'t':0,'b':0,'r':0},
+        mapbox = {
+            'center': {'lon': 10, 'lat': 10},
+            'style': "open-street-map",
+            'center': {'lon': -20, 'lat': -20},
+            'zoom': 1})
+
+    plot_url = py.plot(fig)
+    return(plot_url)
+    
+    # fig.create_html("templates/check.html")
+
+result = createMap(makeGraph(parse("test.txt")))
+print(result)
