1a4cdcd359de388ff503d2962508135d35260834
jerwansyah <blcktmyie123@gmail.com>
2021-04-06 12:48:41 +0700

    progress

--- src/app.py
+++ src/app.py
@@ -1,62 +1,48 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import g as gobj
 from flask_bootstrap import Bootstrap
 import plotly.graph_objects as go
 from werkzeug.utils import secure_filename
 import os
 from astar import *
+from plotting import *
 
 UPLOAD_FOLDER = '../test/'
 ALLOWED_EXTENSIONS = {'txt'}
 
 app = Flask(__name__)
+test = app.app_context()
+test.push()
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 Bootstrap(app)
 
-result = None
-filename = str()
-nodeA = []
-nodeB = []
-src = ''
-dest = ''
-
-def dibaging(string):
-    print('********** ' + string + ' **********')
-    print(nodeA)
-    print(nodeB)
-    print(namafile)
-    print(A)
-    print(B)
+gobj.result = None
+gobj.nodeA = []
+gobj.nodeB = []
+gobj.src = ''
+gobj.dest = ''
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def getIndexFromName(graph, name):
-    for i in range(len(graph)):
-        if graph[i].name[0] == name:
-            return i
-
-
-# @app.route('/', methods=['GET'])
-# def dropdown():
-#     result = makeGraph(parse(file.filename))
-#     nodeA = [i.name[0] for i in result]
-#     nodeB = [i.name[0] for i in result]
-#     return render_template('main.html', nodeA=nodeA, nodeB=nodeB)
-
 @app.route('/')
-def route(filename=None, nodeA=nodeA, nodeB=nodeA, src=src, dest=dest, result=result):
-    return render_template('main.html', filename=filename, nodeA=nodeA, nodeB=nodeB, src=src, dest=dest, result=result)
+def route()
+    with test:
+        result = gobj.result
+        nodeA = gobj.nodeA
+        nodeB = gobj.nodeB
+        src = gobj.src
+        dest = gobj.dest
+    return render_template('main.html', nodeA=nodeA, nodeB=nodeB, src='src', dest='dest', pranala='')
 
 @app.route('/upload', methods=['POST', 'GET'])
 def uploadFile():
-    global filename, nodeA, nodeB, src, dest, result
-    # global namafile, nodeA, nodeB, result, src, dest
     if request.method == 'POST':
         if 'sabmeet' in request.form:
             # check if the post request has the file part
             if 'file' not in request.files:
                 flash('No file part')
-                return render_template('main.html', filename='No file uploaded', nodeA=nodeA, nodeB=nodeB, src=src, dest=dest, result=result)
+                return render_template('main.html')
             file = request.files['file']
             # if user does not select file, browser also
             # submit an empty part without filename
@@ -74,18 +60,39 @@ def uploadFile():
                 nodeB = [i.name[0] for i in result]
                 src = nodeA[0]
                 dest = nodeB[0]
-    return render_template('main.html', filename=filename, nodeA=nodeA, nodeB=nodeB, src=src, dest=dest, result=result)
-    # return redirect(url_for('route', filename=namafile, nodeA=nodeA, nodeB=nodeB, src=A, dest=B, **request.args))
-
-@app.route('/astar', methods=['POST', 'GET'])
-def viewMap():
-    global filename, nodeA, nodeB, src, dest, result
-    if request.method == 'POST':
+                with test:
+                    gobj.result = result
+                    gobj.nodeA = nodeA
+                    gobj.nodeB = nodeB
+                    gobj.src = nodeA[0]
+                    gobj.dest = nodeB[0]
+                    # print(gobj.result)
+                    # print(gobj.nodeA)
+                    # print(gobj.nodeB)
+                    # print(gobj.src)
+                    # print(gobj.dest)
+            return render_template('main.html', nodeA=nodeA, nodeB=nodeB, src=src, dest=dest, pranala='')
+    if request.method == 'GET':
         if 'getMap' in request.form:
-            src = request.form.get('nA')
-            dest = request.form.get('nB')
-            # createMap(src, dest, result)
-    return render_template('main.html', filename=filename, nodeA=nodeA, nodeB=nodeB, src=src, dest=dest, result=result)
+            with test:
+                gobj.src = request.args.get('src')
+                gobj.dest = request.args.get('dest')
+                pranala = createMap(src, dest, gobj.result)
+            return render_template('main.html', nodeA=nodeA, nodeB=nodeB, src=src, dest=dest, pranala=pranala)
+    # return render_template('main.htmlplotting return render_template('main.html')
+
+# @app.route('/upload', methods=['POST', 'GET'])
+# @app.route('/astar', methods=['POST', 'GET'])
+# def viewMap():
+#     if request.method == 'POST':
+#         if 'getMap' in request.form:
+#             with test:
+#                 gobj.src = request.form.get('src')
+#                 gobj.dest = request.form.get('dest')
+#                 # createMap(src, dest, gobj.result)
+#    return render_template('main.html', nodeA=gobj.nodeA, nodeB=gobj.nodeB, src=gobj.src, dest=gobj.dest)
+#    return render_template('main.html', nodeA=nodeA, nodeB=nodeB, src=src, dest=dest)
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=2211)
+
